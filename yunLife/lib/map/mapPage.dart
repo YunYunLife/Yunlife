a076e065e66adf7f36baa851846f8f8c0050c644
baya <blocktreess@gmail.com>
2024-10-01 00:55:48 +0800

    combuy robot and up load map

--- yunLife/lib/map/mapPage.dart
+++ yunLife/lib/map/mapPage.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart'; // 添加 Google Maps Flutter 包
 import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:yunLife/setting.dart';
 
 class mapPage extends StatefulWidget {
   const mapPage({super.key});
@@ -11,15 +16,25 @@ class mapPage extends StatefulWidget {
 
 class _mapPageState extends State<mapPage> {
   final locationController = Location();
+  
   static const googlePlex = LatLng(23.693809679486208, 120.53182880511123);
   static const goatlocation = LatLng(23.6947643780979, 120.5378082675644);
-  LatLng? currentPosition;
+  LatLng? currentPosition = null;
+
+  Map<PolylineId,Polyline> polylines = {};
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) async => await fetchLocationUpdates());
+    WidgetsBinding.instance 
+        .addPostFrameCallback((_) async => await initializeMap());
+  }
+
+  Future<void> initializeMap() async{
+    int i=1;
+    await fetchLocationUpdates(i);
+    final coordinates = await fetchPolylinePoints();
+    generatePolyLineFromPoints(coordinates);
   }
 
   @override
@@ -27,7 +42,7 @@ class _mapPageState extends State<mapPage> {
     return Scaffold(
       body: currentPosition == null
           ? const Center(
-              child: CircularProgressIndicator(),
+              child: Text("Loading..."),
             )
           : GoogleMap(
               initialCameraPosition: CameraPosition(
@@ -45,14 +60,15 @@ class _mapPageState extends State<mapPage> {
                     position: googlePlex),
                 const Marker(
                     markerId: MarkerId('goat'),
-                    icon: BitmapDescriptor.defaultMarker,
+                    icon: BitmapDescriptor.defaultMarker, 
                     position: goatlocation)
               },
+              polylines: Set<Polyline>.of(polylines.values),
             ),
     );
   }
 
-  Future<void> fetchLocationUpdates() async {
+  Future<void> fetchLocationUpdates(int i) async {
     bool serviceEnabled;
     PermissionStatus permissionStatus;
 
@@ -70,7 +86,7 @@ class _mapPageState extends State<mapPage> {
         return;
       }
     }
-    locationController.onLocationChanged.listen((currentLocation) {
+    locationController.onLocationChanged.listen((LocationData currentLocation) {
       if (currentLocation.latitude != null &&
           currentLocation.longitude != null) {
         setState(() {
@@ -78,41 +94,50 @@ class _mapPageState extends State<mapPage> {
             currentLocation.latitude!,
             currentLocation.longitude!,
           );
+          print(currentPosition);
         });
-        print(currentPosition!);
       }
     });
   }
 
-//   Future<List<LatLng>> fetchPolylinePoints() async {
-//     final polylinePoints = PolylinePoints();
+  Future<List<LatLng>> fetchPolylinePoints() async {
+    final polylinePoints = PolylinePoints();
+    
+    if(currentPosition == null){
+      print("fuck\n");
+    }
+    print(currentPosition);
 
-//     final result = await polylinePoints.getRouteBetweenCoordinates(
-//       googleMapsApiKey,
-//       PointLatLng(googlePlex.latitude, googlePlex.longitude),
-//       PointLatLng(mountainView.latitude, mountainView.longitude),
-//     );
+    final result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey:GOOGLE_MAPS_API_KEY,
+      request: PolylineRequest( 
+        origin: PointLatLng(googlePlex.latitude, googlePlex.longitude),
+        destination: PointLatLng(goatlocation.latitude, goatlocation.longitude),
+        mode: TravelMode.walking, 
+      ),
+    );
 
-//     if (result.points.isNotEmpty) {
-//       return result.points
-//           .map((point) => LatLng(point.latitude, point.longitude))
-//           .toList();
-//     } else {
-//       debugPrint(result.errorMessage);
-//       return [];
-//     }
-//   }
+    if (result.points.isNotEmpty) {
+      return result.points
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+    } else {
+      debugPrint(result.errorMessage);
+      return [];
+    }  
+  }
 
-//   Future<void> generatePolyLineFromPoints(
-//       List<LatLng> polylineCoordinates) async {
-//     const id = PolylineId('polyline');
+  Future<void> generatePolyLineFromPoints(
+      List<LatLng> polylineCoordinates) async {
+    const id = PolylineId('polyline');
 
-//     final polyline = Polyline(
-//       polylineId: id,
-//       color: Colors.blueAccent,
-//       points: polylineCoordinates,
-//       width: 5,
-//     );
+    final polyline = Polyline(
+      polylineId: id,
+      color: Colors.blueAccent,
+      points: polylineCoordinates,
+      width: 5,
+    );
 
-//     setState(() => polylines[id] = polyline);
+    setState(() => polylines[id] = polyline);
+}
 }
