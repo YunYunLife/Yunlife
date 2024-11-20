12557392e9fdc2856f0bc4d939ce4f6cca14e834
Ben <blocktreess@gmail.com>
2024-11-20 17:53:00 +0800

    查詢地點完成

--- yunLife/lib/Page/map/mapPage.dart
+++ yunLife/lib/Page/map/mapPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:location/location.dart';
+import 'package:yunLife/Page/map/getGoat.dart';
 import 'package:yunLife/setting.dart';
 
 class mapPage extends StatefulWidget {
@@ -14,21 +15,31 @@ class mapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<mapPage> {
-  final myGoat = TextEditingController();
+  LatLng? goatLocation;
+  String message = "未輸入資料";
 
-  String goat() {
-    String text = myGoat.text;
-    if (text.length < 3) {
-      return "學校沒有這教室";
-    } else  {
-      return text.substring(0, 2)+" "+text.substring(2, 3)+" 樓地圖";
-    } 
+  Future<void> goat(text) async {
+    if (!(text.length < 3)) {
+      message = text.substring(0, 2) + " " + text.substring(2, 3) + " 樓地圖";
+      LatLng? location = await getPointFromServer(text.substring(0, 2));
 
+      if (location != null) {
+        setState(() {
+          goatLocation = location;
+        });
+        // 更新地图标记和视角
+        _updateMapView(location);
+      }
+    }
+       message =  "學校沒有這教室";
+  }
+
+  void _updateMapView(LatLng location) {
+    mapController.animateCamera(CameraUpdate.newLatLng(location));
   }
 
   late GoogleMapController mapController;
   final Location location = Location();
-  LatLng goatLocation = const LatLng(23.6947643780979, 120.5378082675644);
   LatLng? currentLocation;
   Map<MarkerId, Marker> markers = {};
   Map<PolylineId, Polyline> polylines = {};
@@ -38,7 +49,7 @@ class _MapPageState extends State<mapPage> {
   @override
   void initState() {
     super.initState();
-    _addMarker(goatLocation, "goatLocation", BitmapDescriptor.defaultMarker);
+
     _initLocation();
   }
 
@@ -68,22 +79,35 @@ class _MapPageState extends State<mapPage> {
     location.onLocationChanged.listen((LocationData locationData) {
       if (locationData.latitude != null && locationData.longitude != null) {
         setState(() {
-          currentLocation = LatLng(locationData.latitude!, locationData.longitude!);
+          currentLocation =
+              LatLng(locationData.latitude!, locationData.longitude!);
         });
-        _getPolyline();
+        if (goatLocation != null) {
+          _getPolyline();
+        }
       }
     });
   }
 
+  void setGoatPoint() {
+    if (goatLocation != null) {
+      _addMarker(goatLocation!, "goatLocation", BitmapDescriptor.defaultMarker);
+    }
+  }
+
   void _addMarker(LatLng position, String id, BitmapDescriptor descriptor) {
     MarkerId markerId = MarkerId(id);
-    Marker marker = Marker(markerId: markerId, icon: descriptor, position: position);
+    Marker marker =
+        Marker(markerId: markerId, icon: descriptor, position: position);
     markers[markerId] = marker;
     setState(() {});
   }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    if (goatLocation != null) {
+      mapController.animateCamera(CameraUpdate.newLatLng(goatLocation!));
+    }
   }
 
   void _addPolyline() {
@@ -105,8 +129,10 @@ class _MapPageState extends State<mapPage> {
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       googleApiKey: GOOGLE_MAPS_API_KEY,
       request: PolylineRequest(
-        origin: PointLatLng(currentLocation!.latitude, currentLocation!.longitude),
-        destination: PointLatLng(goatLocation.latitude, goatLocation.longitude),
+        origin:
+            PointLatLng(currentLocation!.latitude, currentLocation!.longitude),
+        destination:
+            PointLatLng(goatLocation!.latitude, goatLocation!.longitude),
         mode: TravelMode.walking,
       ),
     );
@@ -122,57 +148,55 @@ class _MapPageState extends State<mapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.amber[200],
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: myGoat,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    prefixIcon: Icon(Icons.search),
-                    labelText: "請輸入教室代號",
-                    hintText: "範例：MA214",
-                    hintStyle: TextStyle(color: Colors.grey[700]),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              onChanged: (text){goat(text);},
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[200],
+                prefixIcon: Icon(Icons.search),
+                labelText: "請輸入教室代號",
+                hintText: "範例：MA214",
+                hintStyle: TextStyle(color: Colors.grey[700]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-            ],
+            ),
           ),
-
-          // GoogleMap(
-          //   initialCameraPosition: CameraPosition(
-          //     target: currentLocation ?? LatLng(23.693297406133105, 120.53458268547402),
-          //     zoom: 15,
-          //   ),
-          //   myLocationEnabled: true,
-          //   onMapCreated: _onMapCreated,
-          //   markers: Set<Marker>.of(markers.values),
-          //   polylines: Set<Polyline>.of(polylines.values),
-          // ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text(goat()),
-                    content: Text('this is picture'),
-                  );
-                },
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: currentLocation ??
+                    LatLng(23.693297406133105, 120.53458268547402),
+                zoom: 15,
+              ),
+              myLocationEnabled: true,
+              onMapCreated: _onMapCreated,
+              markers: Set<Marker>.of(markers.values),
+              polylines: Set<Polyline>.of(polylines.values),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(message),
+                content: Text('this is picture'),
               );
             },
-            tooltip: "顯示該樓層地圖",
-            child: Icon(Icons.insert_photo)),
+          );
+        },
+        tooltip: "顯示該樓層地圖",
+        child: Icon(Icons.insert_photo),
       ),
     );
   }
