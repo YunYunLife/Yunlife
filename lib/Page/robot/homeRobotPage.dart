464bccf43c8ed060848b5db84d7fd3362258ee76
Ben <90437825+B11023@users.noreply.github.com>
2024-11-21 16:28:14 +0800

    Update homeRobotPage.dart

--- lib/Page/robot/homeRobotPage.dart
+++ lib/Page/robot/homeRobotPage.dart
@@ -5,34 +5,37 @@ import 'package:speech_to_text/speech_to_text.dart'
     as stt; // Import speech-to-text package
 import 'package:permission_handler/permission_handler.dart'; // Import permission handler for microphone permissions
 import 'package:intl/intl.dart'; // Import for time formatting
+import 'package:yunLife/global.dart';
 import 'package:yunLife/setting.dart'; // Import settings for the API key and server URL
+import 'package:url_launcher/url_launcher.dart'; // Import for launching URLs
 
-// Define the different zstates for the robot's image
+// Define the different states for the robot's image
 enum ImageState { nap, awake, answer, complete }
 
 class homeRobotPage extends StatefulWidget {
   const homeRobotPage({super.key});
 
   @override
-  _homeRobotPageState createState() => _homeRobotPageState();
+  _HomeRobotPageState createState() => _HomeRobotPageState();
 }
 
-class _homeRobotPageState extends State<homeRobotPage> {
-  // Initial state is 'nap'
+class _HomeRobotPageState extends State<homeRobotPage> {
   ImageState _imageState = ImageState.nap;
   bool _isChatVisible = false;
   final TextEditingController _textController = TextEditingController();
   final List<Map<String, dynamic>> _messages = []; // Store messages with time
   bool _isTyping = false; // Instead of _isLoading
   late stt.SpeechToText _speech; // Speech-to-text instance
-  bool _isListening = false;
-  String _speechText = '';
+  bool _isListening = false; // To track mic status
+  String _speechText = ''; // Stores the recognized speech
+  final String _currentLocaleId = 'en_US'; // Default to English
 
   @override
   void initState() {
     super.initState();
     _speech = stt.SpeechToText(); // Initialize speech-to-text
     _checkPermissions(); // Check for microphone permissions
+    _getAvailableLocales(); // Check available languages
   }
 
   // Check microphone permission
@@ -44,30 +47,47 @@ class _homeRobotPageState extends State<homeRobotPage> {
     }
   }
 
-  // Function to handle image tap (nap -> awake)
-  void _onImageTap() {
-    if (_imageState == ImageState.nap) {
-      setState(() {
-        _imageState = ImageState.awake;
-        _isChatVisible = true; // Show the chat interface
-      });
+  // List available locales (languages)
+  Future<void> _getAvailableLocales() async {
+    List<stt.LocaleName> locales = await _speech.locales();
+    print(
+        "Available locales: ${locales.map((locale) => locale.localeId).toList()}");
+  }
+
+  Future<String> _getChatResponse(String userInput) async {
+    final response = await http.post(
+      Uri.parse('$SERVER_IP/ask'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'prompt': userInput, 'username': globalUsername}),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      return responseData['response'];
+    } else if (response.statusCode == 401) {
+      return "請先登錄系統已使用個人行事曆功能。";
+    } else {
+      throw Exception('Failed to load  response from server');
     }
   }
 
-  // Function to start/stop speech recognition
+  // Start/stop speech recognition
   void _startListening() async {
-    // Ensure microphone permissions are granted
     var permissionStatus = await Permission.microphone.status;
     if (!permissionStatus.isGranted) {
-      await Permission.microphone
-          .request(); // Request permission if not granted
+      await Permission.microphone.request();
       return;
     }
 
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (status) => print('onStatus: $status'),
-        onError: (errorNotification) => print('onError: $errorNotification'),
+        onError: (errorNotification) {
+          print('onError: $errorNotification');
+          setState(() {
+            _isListening = false;
+          });
+        },
       );
 
       if (available) {
@@ -77,15 +97,12 @@ class _homeRobotPageState extends State<homeRobotPage> {
 
         _speech.listen(
           onResult: (val) => setState(() {
-            _speechText = val.recognizedWords;
-            _textController.text =
-                _speechText; // Update the input field with recognized text
+            _speechText = val.recognizedWords; // Capture recognized words
+            print("Recognized words: $_speechText"); // Debugging
+            _textController.text = _speechText; // Update the input field
           }),
-          listenFor: const Duration(seconds: 10), // Set listening duration
-          pauseFor: const Duration(
-              seconds: 5), // Time to wait for a pause before stopping
-          cancelOnError: true,
-          partialResults: false,
+          localeId: _currentLocaleId,
+          partialResults: true,
         );
       } else {
         setState(() {
@@ -100,27 +117,66 @@ class _homeRobotPageState extends State<homeRobotPage> {
     }
   }
 
+  // Function to handle image tap (nap -> awake)
+  void _onImageTap() {
+    if (_imageState == ImageState.nap) {
+      setState(() {
+        _imageState = ImageState.awake;
+        _isChatVisible = true; // Show the chat interface
+      });
+    }
+  }
+
+  // Function to open a map URL
+  Future<void> openMap(String url) async {
+    Uri mapUrl = Uri.parse(url);
+
+    if (await canLaunchUrl(mapUrl)) {
+      await launchUrl(mapUrl,
+          mode: LaunchMode.externalApplication); // Open map in Google Maps
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   // Function to send a message
   void _sendMessage() async {
     if (_textController.text.isEmpty) return;
+
+    // Unfocus the text field to hide the keyboard
+    FocusScope.of(context).unfocus();
 
     String userInput = _textController.text;
     String time = DateFormat('HH:mm').format(DateTime.now()); // Format time
 
     setState(() {
       _messages.add({'text': 'You: $userInput', 'time': time});
-      _isTyping = true; // Show Yun Yun typing indicator
+      _isTyping = true;
       _imageState = ImageState.answer;
     });
 
     _textController.clear();
     try {
       String response = await _getChatResponse(userInput);
-      setState(() {
-        String time = DateFormat('HH:mm').format(DateTime.now());
-        _messages.add({'text': 'Robot: $response', 'time': time});
-        _imageState = ImageState.complete;
-      });
+
+      // Check if the response contains a map link
+      if (response.contains('https://www.google.com/maps')) {
+        setState(() {
+          String time = DateFormat('HH:mm').format(DateTime.now());
+          _messages.add({
+            'text': 'Robot: Click to open map',
+            'time': time,
+            'url': response
+          });
+          _imageState = ImageState.complete;
+        });
+      } else {
+        setState(() {
+          String time = DateFormat('HH:mm').format(DateTime.now());
+          _messages.add({'text': 'Robot: $response', 'time': time});
+          _imageState = ImageState.complete;
+        });
+      }
     } catch (e) {
       setState(() {
         String time = DateFormat('HH:mm').format(DateTime.now());
@@ -129,24 +185,8 @@ class _homeRobotPageState extends State<homeRobotPage> {
       });
     } finally {
       setState(() {
-        _isTyping = false; // Remove Yun Yun typing indicator
+        _isTyping = false;
       });
-    }
-  }
-
-  // Function to get ChatGPT response from the server
-  Future<String> _getChatResponse(String userInput) async {
-    final response = await http.post(
-      Uri.parse('$SERVER_IP/ask'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'prompt': userInput}),
-    );
-
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      return responseData['response'];
-    } else {
-      throw Exception('Failed to load response from server');
     }
   }
 
@@ -190,58 +230,44 @@ class _homeRobotPageState extends State<homeRobotPage> {
           SingleChildScrollView(
             child: Center(
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(6.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const SizedBox(height: 5),
                     GestureDetector(
                       onTap: _onImageTap, // Handle image tap
                       child: SizedBox(
-                        height: 300,
-                        width: 300,
+                        height: 280,
+                        width: 280,
                         child: Image.asset(
                           _getImageAsset(),
                           fit: BoxFit.cover,
                         ),
                       ),
                     ),
-                    const SizedBox(height: 2),
-                    // Show chat interface if _isChatVisible is true
+                    const SizedBox(height: 5),
                     if (!_isChatVisible)
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "點選助理以喚醒",
-                                style: TextStyle(
-                                    fontSize: 24, color: Colors.grey[400]),
-                              ),
-                            ],
-                          ),
-                        ],
+                      Text(
+                        '點擊助理以喚醒',
+                        style:
+                            TextStyle(fontSize: 24, color: Color(0xFF9B979C)),
                       )
                     else
                       Column(
                         children: [
                           Container(
-                            height: MediaQuery.of(context).size.height * 0.3,
+                            height: 265,
                             decoration: BoxDecoration(
                               border: Border.all(color: Colors.brown),
                               borderRadius: BorderRadius.circular(8.0),
                             ),
                             child: ListView.builder(
-                              reverse:
-                                  true, // Show latest messages at the bottom
+                              reverse: true,
                               padding: const EdgeInsets.all(8.0),
-                              itemCount: _messages.length +
-                                  (_isTyping
-                                      ? 1
-                                      : 0), // Add 1 for typing indicator if active
+                              itemCount: _messages.length + (_isTyping ? 1 : 0),
                               itemBuilder: (context, index) {
                                 if (index == 0 && _isTyping) {
-                                  return _buildTypingIndicator(); // Show typing indicator
+                                  return _buildTypingIndicator();
                                 }
                                 final message = _messages[_messages.length -
                                     1 -
@@ -249,6 +275,25 @@ class _homeRobotPageState extends State<homeRobotPage> {
                                     (_isTyping ? 1 : 0)];
                                 bool isUser =
                                     message['text'].startsWith('You:');
+
+                                if (message.containsKey('url')) {
+                                  return Align(
+                                    alignment: isUser
+                                        ? Alignment.centerRight
+                                        : Alignment.centerLeft,
+                                    child: GestureDetector(
+                                      onTap: () => openMap(message['url']),
+                                      child: Text(
+                                        'Click to open map',
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }
+
                                 return Align(
                                   alignment: isUser
                                       ? Alignment.centerRight
@@ -264,8 +309,12 @@ class _homeRobotPageState extends State<homeRobotPage> {
                                             vertical: 5.0),
                                         decoration: BoxDecoration(
                                           color: isUser
-                                              ? Colors.brown[100]
-                                              : Colors.brown[200],
+                                              ? Colors.brown[50]
+                                              : Colors.brown[180],
+                                          border: Border.all(
+                                            color: Colors.brown,
+                                            width: 1.0,
+                                          ),
                                           borderRadius:
                                               BorderRadius.circular(8.0),
                                         ),
@@ -273,16 +322,13 @@ class _homeRobotPageState extends State<homeRobotPage> {
                                           message['text']
                                               .substring(isUser ? 4 : 7),
                                           style: TextStyle(
-                                            color: Colors.brown[800],
-                                          ),
+                                              color: Colors.brown[800]),
                                         ),
                                       ),
                                       Text(
                                         message['time'],
                                         style: TextStyle(
-                                          fontSize: 10,
-                                          color: Colors.grey,
-                                        ),
+                                            fontSize: 10, color: Colors.grey),
                                       ),
                                     ],
                                   ),
@@ -290,11 +336,9 @@ class _homeRobotPageState extends State<homeRobotPage> {
                               },
                             ),
                           ),
-                          const SizedBox(
-                              height: 10), // Add space between chat and input
+                          const SizedBox(height: 8),
                           Padding(
-                            padding: const EdgeInsets.only(
-                                left: 62.0), // 左邊添加 60 像素的內邊距
+                            padding: const EdgeInsets.only(left: 62.0),
                             child: Row(
                               children: [
                                 Expanded(
@@ -311,8 +355,7 @@ class _homeRobotPageState extends State<homeRobotPage> {
                                 IconButton(
                                   icon: const Icon(Icons.send),
                                   color: Colors.brown[300],
-                                  onPressed:
-                                      _sendMessage, // Disable button while loading
+                                  onPressed: _sendMessage,
                                 ),
                               ],
                             ),
@@ -324,24 +367,22 @@ class _homeRobotPageState extends State<homeRobotPage> {
               ),
             ),
           ),
-          // 麥克風圖示應該顯示在左下角，當且僅當圖片狀態是 "awake"
           if (_imageState != ImageState.nap)
             Positioned(
               bottom: 10,
-              left: 10, // 將麥克風圖示移到左下角
+              left: 10,
               child: GestureDetector(
-                onTap: _startListening, // Start listening when mic is tapped
+                onTap: _startListening,
                 child: Container(
-                  padding:
-                      const EdgeInsets.all(10.0), // Padding around the icon
+                  padding: const EdgeInsets.all(10.0),
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Colors.grey[300], // 圓形背景顏色
+                    color: Colors.grey[300],
                     boxShadow: const [
                       BoxShadow(
                         color: Colors.black26,
                         blurRadius: 5,
-                        offset: Offset(0, 2), // 阴影偏移
+                        offset: Offset(0, 2),
                       ),
                     ],
                   ),
