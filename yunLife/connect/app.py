dfb22e6fedee59c2f245a5b9c6466b98b32d6899
esther1qaz <146061392+esther1qaz@users.noreply.github.com>
2024-11-21 14:27:22 +0800

    Update app.py

--- yunLife/connect/app.py
+++ yunLife/connect/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, make_response
 import json
-from yunLife.lib.mongoConnect import getCollection, collectionNames
+from mongoConnect import getCollection, collectionNames
 
 app = Flask(__name__)
 
